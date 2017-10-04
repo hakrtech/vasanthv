97c6d297d4c14558453fae4d6ea36f4abfd14926
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2017-10-04 23:05:47 +0530

    Kochan Chapter8

---
+++ Kochan_Programming_in_C/Chapter8/Chapter8exercises/ch8exercise9.c
@@ -0,0 +1,46 @@
+/* Chapter 8 Exercise 9 Program to find Least Common Multiple(LCM) of two postive number. The LCM(u,v) = uv / GCD(u,v) and  u,v >=0.
+ * Vasanth 04 October 2017
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int gcd(int u, int v);
+
+	int u;
+	int v;
+	int result;
+	int gcd1;
+
+	printf("Welcome!!! to Least Common Multiple[LCM] of any two numbers:\n");
+	printf("Enter U value:  ");
+	scanf("%i", &u);
+	printf("Enter V value:  ");
+	scanf("%i", &v);
+
+	if(u >= 0 || v >= 0)
+	{
+		gcd1 = gcd(u, v);
+		result =  (u * v) / gcd1;
+	}
+
+	printf("\nThe Greatest Common Divisor[GCD] of U and V:  %i and The Least Common Multiple[LCM] of U and V:  %i\n\n", gcd1, result);
+
+	return 0;
+}
+
+int gcd(int u, int v)
+{
+	int temp;
+
+	while( v != 0)
+	{
+		temp = u % v;
+		u = v;
+		v = temp;
+	}
+
+	return u;
+}
+
