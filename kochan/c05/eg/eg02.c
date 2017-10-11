e8c64184a62b243850e8b0c17039259987d2d05c
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2017-10-11 12:48:16 +0530

    eg02 completed

--- kochan/c05/eg/eg02.c
+++ kochan/c05/eg/eg02.c
@@ -2,4 +2,19 @@
  * Vasanth 11 October 2017
  */
 
+#include <stdio.h>
 
+int main(void)
+{
+	int num;
+	int triangulnum;
+	num =1;
+	triangulnum = 0;
+
+	while (num <= 200) {
+		triangulnum = triangulnum + num;
+		num += 1;
+	}
+	printf("two hundredth triangular number %i\n", triangulnum);
+	return 0;
+}
