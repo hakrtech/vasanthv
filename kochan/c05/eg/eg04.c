af516bb6afc892f443404e97fa5e3ce9fc39d518
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2017-10-11 14:27:52 +0530

    eg04 completed

---
+++ kochan/c05/eg/eg04.c
@@ -0,0 +1,23 @@
+/* Chapter 05 Example 04 Program to find triangular number using user input
+ * Vasanth 11 October 2017
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int num;
+	int input;
+	int triangul;
+	printf("Give number to get triangular number ");
+	scanf("%i", &input);
+	num = 1;
+	triangul = 0;
+
+	 while (num <= input) {
+		triangul += num;
+		num += 1;
+	}
+	printf("triangular number %i\n", triangul);
+	return 0;
+}
