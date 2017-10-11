86f6037ed43e90024351e1c1c4388a26a2d9be35
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2017-10-11 12:32:20 +0530

    #1 eg01 and Makefile

---
+++ kochan/c05/eg/eg01.c
@@ -0,0 +1,13 @@
+/* Chapter 05 Example 01 Program to calculate eighth triangular number
+ * Vasanth 11 October 2017
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int triangulnum;
+	triangulnum = 1+2+3+4+5+6+7+8;
+	printf("eighth triangular number: %i\n", triangulnum);
+	return 0;
+}
---
+++ kochan/c05/eg/eg02.c
@@ -0,0 +1,5 @@
+/* Chapter 05 Example 02 two hundredth triangular number
+ * Vasanth 11 October 2017
+ */
+
+
