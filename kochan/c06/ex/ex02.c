c4e9313e9aa07f5d42db835ab78ed82769d56223
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2017-10-12 14:18:37 +0530

    #2 ex01 and Makefile completed

---
+++ kochan/c06/ex/ex02.c
@@ -0,0 +1,29 @@
+/* Chapter 06 Exercise 02 Program to test the first number is evenly divisible by the second number
+ * Vasanth 12 October 2017
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int first_num;
+	int second_num;
+	int temp;
+
+	printf("enter any two number and check first number is evenly divisible by second number>\n");
+	printf("enter first number: ");
+	scanf("%i", &first_num);
+	printf("enter second number: ");
+	scanf("%i", &second_num);
+	printf("entered first number is %d and second number is %d\n", first_num, second_num);
+
+	temp = first_num / second_num;
+
+	if (temp == 2) {
+		printf("yes first number %i evenly divisible by the second number %i\n", first_num, second_num);
+	} else {
+		printf("no first number %i not evenly divisible by second number %i\n", first_num, second_num);
+	}
+
+	return 0;
+}
