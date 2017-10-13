d9dfac7a600a514c13a867231796cf3ace57d482
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2017-10-13 20:07:22 +0530

    ex07 modified

--- kochan/c06/ex/ex07.c
+++ kochan/c06/ex/ex07.c
@@ -17,36 +17,35 @@ int main(void)
 	printf("entered number is %i\n", limit);
 
 	if (limit < 2) {
-		printf("sorry this is not valid number to check prime or not\n");
-		exit (1);
-	} else {
-		printf("prime numbers: 2 ");
-
-		i = 3;
-		while (i <= limit) {
-			int j;
-
-			is_prime = true;
-			j = 3;
-			while (j < i) {
-				int remainder;
-
-				remainder = i % j;
-				if (remainder == 0) {
-					is_prime = false;
-					break;		/* exit continuous looping */
-				}
-				++j;
-			}
-
-			if (is_prime == true) {
-				printf("%i ", i);
-			}
-
-			i += 2;		/* now, this loop not check the even numbers */
-		}
-		printf("\n");
+		printf("sorry this is not valid value to check prime or not\n");
+		exit (EXIT_FAILURE);
 	}
+	printf("prime numbers: 2 ");
+
+	i = 3;
+	while (i <= limit) {
+		int j;
+
+		is_prime = true;
+		j = 3;
+		while (j < i) {
+			int remainder;
+
+			remainder = i % j;
+			if (remainder == 0) {
+				is_prime = false;
+				break;		/* exit continuous looping */
+			}
+			++j;
+		}
+
+		if (is_prime == true) {
+			printf("%i ", i);
+		}
+
+		i += 2;		/* now, this loop not check the even numbers */
+	}
+	printf("\n");
 
 	return 0;
 }
