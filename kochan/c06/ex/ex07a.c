d38e90ed4ebb587af7b433b06cc2978f3eeda7bb
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2017-10-13 21:53:11 +0530

    ex07a completed

---
+++ kochan/c06/ex/ex07a.c
@@ -0,0 +1,58 @@
+/* Chapter 06 Exercise 07 Program to generate prime number
+ * Vasanth 13 October 2017
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+void prime_print(int output, bool prime)
+{
+	if (prime == true) {
+		printf("%i ", output);
+	}
+}
+
+void prime_generate(int limit)
+{
+	bool is_prime;
+	int jump;
+	int start;
+
+	for (jump = 3; jump <= limit; jump += 2) {	/* now, this loop not check the even numbers */
+
+		is_prime = true;
+
+		for (start = 3; start < jump; ++start) {
+			int remainder;
+
+			remainder = jump % start;
+			if (remainder == 0) {
+				is_prime = false;
+				break;		/* exit continuous looping */
+			}
+		}
+		prime_print(jump, is_prime);
+	}
+}
+
+int main(void)
+{
+	int input;
+
+	printf("enter number and to get upto prime numbers> ");
+	scanf("%i", &input);
+	printf("entered number is %i\n", input);
+
+	if (input < 2) {
+		printf("sorry this is not valid value to check prime or not\n");
+		exit (EXIT_FAILURE);
+	}
+	printf("prime numbers: 2 ");
+
+	prime_generate(input);
+
+	printf("\n");
+
+	return 0;
+}
