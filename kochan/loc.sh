755e324a840d3a36912cb760f468a2074f582f3b
Dinesh <dinesh.thirumurthy@gmail.com>
2017-10-15 19:57:15 +0530

    #2 loc counter

--- kochan/loc.sh
+++ kochan/loc.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-LOC=$(find . -name "*.c" -print | xargs wc -l | tail -1 | awk '{ print $1; }')
+NFILES=$(find . -type f -name "*.[hc]" -print | wc -l)
+LOC=$(find . -name "*.[ch]" -print | xargs wc -l | tail -1 | awk '{ print $1; }')
+CLOC=$(find . -type f -name "*.[hc]" -print | xargs cat | sed -e '/^[ 	]*$/d' | wc -l)
 TLOC=5000
-echo "Lines of Code:   $LOC"
+echo "Lines of Code:   $LOC ($CLOC non ws) in $NFILES files"
 echo "Target LOC:      $TLOC"
 PCT=$(echo "$LOC*100/$TLOC" | bc)
 echo "Target Achieved: $PCT%"
