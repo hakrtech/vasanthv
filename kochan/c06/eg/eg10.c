69a2f808be11156f0cdd2ff30090c34d9e7f39f8
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2017-10-15 22:12:20 +0530

    #2 eg10 completed

---
+++ kochan/c06/eg/eg10.c
@@ -0,0 +1,33 @@
+/* Chapter 06 Example 10 Program to generate a table of prime numbers
+ * Vasanth 15 October 2017
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	int j;
+
+	printf("generationg first 100 prime number> 2 ");
+
+	for (i = 3; i <= 100; i += 2) {
+		_Bool is_prime;
+
+		is_prime = 1;
+		for (j = 3; j < i; j += 2) {
+
+			if (i % j == 0) {
+				is_prime = 0;
+				break;
+			}
+		}
+
+		if ( is_prime != 0) {
+			printf("%i ", i);
+		}
+	}
+	printf("\n");
+
+	return 0;
+}
