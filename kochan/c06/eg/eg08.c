45901bbdf19057c3493b0527f3b6be25bbe730e0
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2017-10-15 19:53:21 +0530

    #2 eg08 completed

---
+++ kochan/c06/eg/eg08.c
@@ -0,0 +1,47 @@
+/* Chapter 06 Example 08 Program to evaluate simple expressions
+ * Vasanth 15 October 2017
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	float val_one;
+	float val_two;
+	char operate;
+
+	printf("type in your expression\n");
+	scanf("%f %c %f", &val_one, &operate, &val_two);
+	printf("entered expression is %.2f %c %.2f\n", val_one, operate, val_two);
+	printf("expression result> ");
+
+	if (operate == '+') {
+		float sum;
+
+		sum = val_one + val_two;
+		printf("%.2f\n", sum);
+
+	} else if (operate == '-') {
+		float difference;
+
+		difference = val_one - val_two;
+		printf("%.2f\n", difference);
+
+	} else if (operate == '*') {
+		float product;
+
+		product = val_one * val_two;
+		printf("%2f\n", product);
+
+	} else if (operate == '/') {
+		float quotient;
+
+		quotient = val_one / val_two;
+		printf("%.2f\n", quotient);
+
+	} else {
+		printf("invalid operation\n");
+	}
+
+	return 0;
+}
