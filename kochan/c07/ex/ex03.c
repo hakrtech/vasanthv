8b450f3bebcc2a53a07f4726007f31b9043b1ff9
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2017-10-31 15:39:23 +0530

    #3 ex03.c modified

--- kochan/c07/ex/ex03.c
+++ kochan/c07/ex/ex03.c
@@ -4,45 +4,54 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 
 int main(void)
 {
 	int counts[10];
 	int i;
-	int num_response;
+	int num_response = 1;
 	int response;
 
-	printf("enter how many number of responses you want (between 1 and 999)?>  ");
-	scanf("%i", &num_response);
-	printf("entered number of response is %i\n", num_response);
+	printf("how many number of responses you want? (use 999 to exit)>\n");
 
-	assert(num_response > 0 && num_response <= 999);
-
-	if (num_response == 999) {
-		printf("you are entered last number of response: %i\n", num_response);
-		exit(EXIT_FAILURE);
-	}
-	
 	for (i = 1; i <= 10; ++i) {
 		counts[i] = 0;
 	}
 
-	printf("enter your responses (between 1 and 10)>\n");
+	while (num_response != 0) {
+		printf("enter 999 to exit or continue any number>\n");
+		scanf("%i", &num_response);
+		printf("entered number of response is %i\n", num_response);
 
-	for (i = 1; i <= num_response; ++i) {
-		scanf("%i", &response);
-		printf("entered response is %i\n", response);
+		if (num_response <= 0) {
+			printf("invalied number!!!\n");
+			exit(1);
+		} else if (num_response == 999) {
+			printf("you are entered exit value %i\n", num_response);
+			break;
+		} else {
+			printf("enter your responses (between 1 and 10)>\n");
+			
+			for (i = 1; i <= num_response; i++) {
+				scanf("%i", &response);
+				printf("entered response is %i\n", response);
 
-		assert(response > 0 && response <= 10);
-		++counts[response];
+				if (response >= 1 && response <= 10) {
+					++counts[response];
+				} else if (response == 999) {
+					printf("entered last response is %i\n", response);
+					break;
+				} else {
+					printf("bad!!! response %i\n", response);
+				}
+			}
+		}
 	}
 
 	printf("\n\n");
 	printf("rating		number of response\n");
 
 	for (i = 1; i <= 10; ++i) {
-		assert(counts[i] < 999);
 		printf("%i		%i\n", i, counts[i]);
 	}
 
