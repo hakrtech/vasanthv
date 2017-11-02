87392b660820b72a1c9545ce96140ee84c64e8fc
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2017-11-02 22:20:42 +0530

    #3 fixed ex07.c

--- kochan/c07/ex/ex07.c
+++ kochan/c07/ex/ex07.c
@@ -1,78 +1,38 @@
-/* Chapter 07 Exercise 07 Program to generate prime numbers using algorithm
- * Vasanth 01 November 2017
+/* Chapter 07 Exercise 07 Program to generate prime numbers using arrays
+ * Vasanth 02 November 2017
  */
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
 
-#include "level.h"
+#define LIMIT 1000
+#define PRIME_TRUE 0	/* 0 is prime */
+#define PRIME_FALSE 1	/* 1 is composite */
 
-#define PRIME (1)
-#define COMPOSITE (0)
+int p[LIMIT];	/* array of declaration values is initialy zero into outside main function */ 
 
 int main(void)
 {
-	int p[LIMIT];
-	int limit = LIMIT;
-	int arrow = 2;
-	int i;
-
-
-	p[0] = COMPOSITE;
-	p[1] = COMPOSITE;
-	
-	printf("int max = %d\n",INT_MAX);
-	printf("sizeof int = %u\n",sizeof(int));
-	printf("sizeof short = %u\n",sizeof(short));
-	printf("sizeof long = %u\n",sizeof(long));
-	printf("sizeof char = %u\n",sizeof(char));
-	printf("short max = %d\n", SHRT_MAX);
-	printf("long max = %lu\n", LONG_MAX);
-	printf("long long max = %llu\n\n", LLONG_MAX);
-	
-	for (i = 2; i <= limit; ++i) {
-		p[i] = PRIME;
-	}
-
-	while (arrow <= limit) {
-	
-		if (p[arrow] == PRIME) {
-			
-			for (i = 2; i <= limit; ++i) {
-			
-				if (p[arrow] == PRIME) {
-					int j;
-
-					j = arrow * i;
-
-					if (j < 0) {
-						break;
-					}
-					if(j > limit) {
-						break;
-					}
-
-					if (j < limit && p[j] == PRIME) {
-						p[j] = COMPOSITE;
-					}
-				}
-			 }
-
-		}
-
-		++arrow;
-	}
+	int arrow;
 
 	printf("prime numbers>\n");
+	
+	for (arrow = 2; arrow < LIMIT; ++arrow) {
+		int jump = 2 * arrow;
 
-	for (i = 2; i <= limit; ++i) {
-		if(p[i] == PRIME) {
-			printf("%i ", i);
+		while (jump < LIMIT) {
+			
+			if (p[jump] == PRIME_TRUE) {
+				p[jump] = PRIME_FALSE;
+			}
+			jump += arrow;
+		}
+		
+		if(p[arrow] == PRIME_TRUE) {
+			printf("%d ", arrow);
 		}
 	}
 	printf("\n");
-	
+
 	return 0;
 }
 
