b11799c37eb71448f83561e8f2cb6630c966e11d
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2017-11-04 15:35:45 +0530

    #3 ex03 fixed

--- kochan/c07/ex/ex03.c
+++ kochan/c07/ex/ex03.c
@@ -3,9 +3,10 @@
  */
 
 #include <stdio.h>
+#include <stdbool.h>
 #include "level.h"
 
-#define INIT_VALUE (0)
+#define INIT_VALUE (-1)
 
 int main(void)
 {
@@ -19,8 +20,13 @@ int main(void)
 		logic[i] = INIT_VALUE;
 	}
 
+#ifdef DEBUG	
 	for (i = 0; i < LIMIT; ++i) {
-		
+		printf("%d ", logic[i]);
+	}
+	printf("\n");
+#endif
+	while(true) {
 		scanf("%d", &input);
 		printf(" %d\n", input);
 		
@@ -28,19 +34,20 @@ int main(void)
 			printf("input over!!!\n");
 			break;
 		}
-		
-		if (input < 0 || input > LIMIT-1){
+		if (input < 0) {
+			printf("bad input %d\n", input);
+		}
+		if (input > LIMIT) {
 			printf("bad input %d\n", input);
 		} else {
-			++logic[input];
-		}		
-
+			logic[input] += 1;
+		}
 	}
 
 	printf("number	count of response\n");
 
 	for (i = 0; i < LIMIT; ++i) {
-		printf("%d	%d\n", i, logic[i]);
+		printf("%d	%d\n", i, logic[i]+1);
 	}
 
 	return 0;
