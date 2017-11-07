fc1596a6c0f4e4b87697dbbf51f21c9164a6e727
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2017-11-07 16:03:05 +0530

    #4 eg11 modified

--- kochan/c08/eg/eg11.c
+++ kochan/c08/eg/eg11.c
@@ -1,35 +1,35 @@
-/* Chapter 8 Example 11 Program to change the array elements in function
+/* Chapter 8 Example 11 Program to change the array elements into function and double the values
  * Vasanth 02 October 2017
  */
 
 #include <stdio.h>
 
+void point_val(float farray[], int limit)
+{
+	int count = 0;
+
+	while (count < limit) {
+		farray[count] *= 2.0;
+		++count;
+	}
+
+}
+
 int main(void)
 {
-	float fvalue[10] = { 8.09f, 8.90f, 9.98f, 8.589, 5.65, 0.999f, 8.97f, 0.999, 77.888, 67.098f };
-	int i;
+	float fvalue[10] = { 8.09, 8.90, 9.98, 8.589, 5.65, 0.999, 8.97, 0.999, 77.888, 67.098 };
+	int count = 0;
 
-	void pointvalue(float x[], int numofarray);
+	printf("multiplied array float value>\n{ ");
 
-	printf("Multipliyed Array Float Value is:\n");
+	point_val(fvalue, 10);
 
-	pointvalue(fvalue, 10);
-
-	for(i = 0; i < 10; i++)
-	{
-		printf("%.3f\n", fvalue[i]);
+	while (count < 10) {
+		printf("%.3f, ", fvalue[count]);
+		++count;
 	}
+	printf("}\n");
 
 	return 0;
 }
 
-void pointvalue(float x[], int numofarray)
-{
-	int i;
-
-	for( i = 0; i < numofarray; ++i)
-	{
-		x[i] *= 2;
-	}
-
-}
