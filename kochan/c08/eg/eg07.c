27ad273ff54d1d8ba4e61a62cb1b7cc3472632da
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2017-11-07 14:54:37 +0530

    #4 eg07 modified

--- kochan/c08/eg/eg07.c
+++ kochan/c08/eg/eg07.c
@@ -4,43 +4,41 @@
 
 #include <stdio.h>
 
-int main(void)
+float absolute_val(float a)
 {
-	float absoluteValue(float a);
-
-	float f1 = -15.5;
-	float f2 = 20;
-	float f3 = -5;
-	int l = -221;
-	float result;
-
-	result = absoluteValue(f1);
-
-	printf("result = %.2f\n", result);
-	printf("f1 = %.2f\n", f1);
-
-	result = absoluteValue(f2) + absoluteValue(f3);
-
-	printf("result = %.2f\n", result);
-
-	result = absoluteValue((float) l);
-
-	printf("result = %.2f\n", result);
-
-	result = absoluteValue(l);
-
-	printf("result = %.2f\n", result);
-	printf("%2.f\n", absoluteValue(-6.0) / 4);
-
-	return 0;
-}
-
-float absoluteValue(float a)
-{
-	if(a < 0)
-	{
+	if (a < 0) {
 		a = -a;
 	}
 
 	return a;
 }
+
+int main(void)
+{
+	float x = -15.5;
+	float y = 20;
+	float z = -5;
+	int num = -221;
+	float result;
+
+	result = absolute_val(x);
+
+	printf("result = %.2f\n", result);
+	printf("x = %.2f\n", x);
+
+	result = absolute_val(y) + absolute_val(z);
+
+	printf("result = %.2f\n", result);
+
+	result = absolute_val((float) num);
+
+	printf("result = %.2f\n", result);
+
+	result = absolute_val(num);
+
+	printf("result = %.2f\n", result);
+	printf("absolute value %2.f\n", absolute_val(-6.0) / 4.0);
+
+	return 0;
+}
+
