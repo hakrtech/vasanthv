52d65b0b51e6e1122b584ad56500d047c5920513
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2017-11-12 16:35:40 +0530

    #4 libreal

--- kochan/libcommon/libnumd.c
+++ kochan/libcommon/libnumd.c
@@ -1,4 +1,6 @@
 
+#include <assert.h>
+
 #include "libnumd.h"
 
 double numd_absd(double d) // TODO
@@ -14,6 +16,9 @@ double numd_sqrtd(double d, double accuracy) // TODO
 {
 	double guess;
 
+	assert(d > 0.0);
+	assert(accuracy > 0.0);
+
 	guess = 1.0;
 	while(numd_absd(guess * guess - d) >= accuracy) {
 		guess = (d / guess + guess) / 2.0;
@@ -25,6 +30,8 @@ double numd_sqrtd(double d, double accuracy) // TODO
 double numd_sqrtd1(double d)
 {
 	double accuracy = 0.000001;
+
+	assert(d > 0.0);
 
 	return numd_sqrtd(d, accuracy);
 }
