afe11dd0de97aee0bba1c440c028376fc1645c69
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2017-11-13 19:26:20 +0530

    #4 libnumd.h

--- kochan/libcommon/libnumd.h
+++ kochan/libcommon/libnumd.h
@@ -1,5 +1,5 @@
 
-double numd_absd(double d); // TODO
-double numd_sqrtd(double d, double accuracy); // TODO
-double numd_sqrtd1(double d); // TODO
+double numd_absd(double d); // DONE
+double numd_sqrtd(double d, double accuracy); // DONE
+double numd_sqrtd1(double d); // DONE
 
