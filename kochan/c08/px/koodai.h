e3bccc18bab5520a7ec127cc6a7c0660506b22c6
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2017-11-15 12:58:36 +0530

    #4 px koodai.h

--- kochan/c08/px/koodai.h
+++ kochan/c08/px/koodai.h
@@ -12,7 +12,7 @@ void koodai_add_element(int k[100], int n, int value); // cproto
 [6, 7, 8] - 6 if present -> [7, 8]
 [6, 7, 8] - 9 if present -> [6, 7, 8]
 koodai_remove_elem(6)
-int koodai_remove_element(int k[100], int n, int value)
+int koodai_remove_element(int k[100], int n, int value);
 // if removed returned n - 1
 // if !removed returned n
 
@@ -22,46 +22,46 @@ E = -1
 
 
 [2, 7, 9, 10] U [ 3, 7, 9, 11 ] -> [2, 7, 9, 10, 3, 7, 9, 11] // plus
-koodai_union()
 c = a + b
+void koodai_union(int ak[100], int bk[100], int n, int ck[100], int m);
 
  k, m, c  l       ms mo cp ec
 [2, 7, 9, 10] minus [ 3, 7, 9, 11 ] ->  [ 2, 10 ]// minus
 [2, 7, 7, 9, 10] minus [ 3, 7, 9, 11 ] ->  [ 2, 7, 10 ]// minus
-koodai_minus
+void koodai_minus(int ak[100], int bk[100], int n, int ck[100]);
 
 
 [2, 7, 9, 10] commonitems [ 3, 7, 9, 11 ] ->  [ 7, 9 ]
-koodai_commonof
+void koodai_commonof(int ak[100], int bk[100], int n, int ck[100]);
 
 [2, 7, 9, 10, 3, 7, 9, 11] -> [ 2, 3, 7, 7, 9, 10, 11]
-koodai_sortup
-koodai_sortdown
+void koodai_sortup(int k[100], int n);
+void koodai_sortdown(int k[100], int n);
 
 [ 2, 3, 7, 7, 9, 10, 11] -> [2, 3, 7, 9, 10, 11]
-koodai_make_unique
+void koodai_make_unique(int k[100], int n);
 
 [ 2, 3, 7, 7, 9, 10, 11] -> 7
-koodai_size
+int koodai_size(int k[100], int n);
 
 [ 2, 3, 7, 7, 9, 10, 11] -> []
-koodai_empty()
+void koodai_empty(int k[100], int n);
 koodai_reset()
 koodai_zero()
 koodai_init()
 
 [ 2, 3, 7, 7, 7, 9, 9, 10, 11] -> [7, 9]
-koodai_dupitems
+void koodai_dupitems(int k[100], int n, int dk[100]);
 
 [ 2, 3, 7, 7, 9, 10, 11] check for 3 -> true
 [ 2, 3, 7, 7, 9, 10, 11] check for 12 -> false
-koodai_isitempresent()
+int koodai_isitempresent(int k[100], int n, int val);
 
 [ 2, 3, 7, 7, 9, 10, 11] -> 2 3 7 7 9 10 11
-koodai_print()
+void koodai_print(int k[100], int n);
 
-koodai_max()
-koodai_min()
+int koodai_max(int k[100], int n);
+int koodai_min(int k[100], int n);
 
 10 = [ 1, 2, 5, 10 ] onnu kooda_add(onnu, 10)
 15 = [ 1, 3, 5, 15 ] rendu
