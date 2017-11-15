0e5307a08c7046fdf8bdd834f340a0740ecfffdb
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2017-11-15 19:19:51 +0530

    #4 koodai.h

--- kochan/c08/px/koodai.h
+++ kochan/c08/px/koodai.h
@@ -1,166 +1,80 @@
 /* koodai library - unordered, non unique items in an array */
 
-#define ASIZ (100)
+#define CAPACITY (100)
+
+/* NOTES: a koodai is implemented by an array so 
+ * we need array values, size of the array, current size of the koodai
+ * (values[], capacity, nitems) defines a koodai
+ */
+
+/* struct koodai {
+ 	int kcap = 100; capacity 
+	int kval[100];  values
+	int kn;		number of items in koodai
+   }
+*/
+
+/* OPERATION: create unitialised koodai of maximum size lim
+   -> allocate space for lim items -> return ptr to that 
+   after c11
+   koodai koodai_create(capacity) LATER
+void koodai_allocate(int k[], int kcap);
+*/
+
+/* OPERATION: destroy koodai
+   [2, 3]-> deallocate memory for above koodai
+   after c11 LATER
+void koodai_destroy(int k[], int kcap);
+*/
 
 /* OPERATION: initialise koodai
    -> [] return 0 (size)
    size koodai_init(koodai) 
 */
-int koodai_init(int k[], int klim, int nk); // API DOIT
-/* return 0 i.e the size of the new koodai 
- */
+int koodai_initialise(int k[], int kcap); // API DOIT
+/* return 0 i.e the size of the new koodai */
 
-/* NOTES: a koodai is implemented by an array so 
- * we need array values, size of the array, current size of the koodai
- * (k[], klim, ksiz) defines a koodai
+/* OPERATION: reset koodai
+   [2, 3, 7, 7, 9, 10, 11] -> [] return 0
+   koodai reset items
  */
+int koodai_reset(int k[], int kcap); // API DOIT
+
+/* OPERATION: koodai get numer of items
+   [2, 3, 7, 7, 9, 10, 11] -> 7 return 7 num of items
+   koodai size
+ */
+int koodai_nitems(int k[], int kcap); // API DOIT
+
+/* OPERATION: check koodai empty
+   [] -> if kn == 1 true
+   [1, 3] -> false
+   [1, 2, 3, 4] -> kcap == kn ( 4 == 4) false 
+   boolean koodai_is_empty(koodai) // API DOIT 
+   return 1 koodai empty
+   return 0 koodai half or full
+*/
+bool koodai_empty(int k[], int kcap, int kn); // API DOIT
+
+/* OPERATION: check koodai full
+   [1, 2, 3, 4] -> if kcap == kn ( 4 == 4) true
+   [1, 2]	-> kcap != kn (4 != 2) false
+   []		-> kn == 0 false
+   boolean koodai_is_full(koodai) // API DOIT
+   return 1 koodai full
+   return 0 koodai half or empty
+*/
+bool koodai_full(int k[], int kcap, int kn); // API DOIT
+
+/* OPERATION: print koodai items
+   [2, 3, 7, 7, 9, 10, 11] -> 2 3 7 7 9 10 11
+   koodai print
+*/
+void koodai_print(int k[], int kcap, int kn); // API DOIT
 
 /* OPERATION: add item to koodai
    [] + 6 -> [6] return 1 i.e. size
    [2, 3] + 7 -> [2, 3, 7] return 3
-   size koodai_add_item(koodai , item) 
+   size koodai add item(koodai , item) 
 */
-int koodai_add_item(int k[], int klim, int nk, int value); // API DOIT
-
-/* OPERATION: remove item from koodai
-   [6, 7, 8]    remove 7 if present -> [6, 8]    return 2
-   [6, 7, 7, 8] remove 7 if present -> [6, 7, 8] return 3
-   [6, 7, 8]    remove 9 if present -> [6, 7, 8] return 3
-   size koodai_remove_item(koodai, item) 
- */
-int koodai_remove_item(int k[], int klim, int nk, int value); // API DOIT
-/* return n-1 if element removed 
-   return n   if element !removed
- */
-
-/* OPERATION: add two koodais 
-   c = a + b
-   a[2, 7, 9, 10] + b[3, 7, 9, 11] -> c[2, 7, 9, 10, 3, 7, 9, 11] return 8
-   a[1, 1, 2, 12] + b[2, 2, 9, 12] -> c[1, 1, 2, 11, 2, 2, 9, 12] return 8
-   return size of koodai c i.e. nc = na + nb
-   size koodai_add(koodai a, koodai koodai b, koodai c)
- */
-int koodai_add( int a[], int alim, int na,		// API DOIT
-		int b[], int blim, int nb,		// API DOIT
-		int c[], int clim);			// API DOIT
-
-/* OPERATION: subtract two koodais i.e. remove matching items of 2nd koodai from first koodai and store in third koodai
-   c = a - b
-   [2, 7, 9, 10]    minus [3, 7, 9, 11] ->  [2, 10]    return 2
-   [2, 7, 7, 9, 10] minus [3, 7, 9, 11] ->  [2, 7, 10] return 3
-   return size of koodai c i.e. nc = na - nb
-   size koodai_minus(kodai a, kodai b, kodai c)
-*/
-int koodai_subtract_koodai(int a[], int alim, int na,
-		  	 int b[], int blim, int nb,
-		  	 int c[], int clim);
-/* return size of koodai c i.e. nc = na + nb
- */
-
-
-/* OPERATION: commonitems items of two koodais
-   [2, 7, 9, 10]    commonitems [3, 7, 9, 11]   -> [7, 9] return 2
-   [4, 8, 4, 6, 2]  commonitems [4, 5, 6, 7, 8] -> [ 
-
-   A[4, 8, 4, 6, 2] 
- = A[2, 4, 4, 6, 8] 
-   B[4, 5, 6, 7, 8]
-
-   A[2, 4, 4,    6,    8] 
-   B[   4,    5, 6, 7, 8]
- A.B[   4,       6,    8]
-
-   [4, 8, 4, 6, 2] commonitems [4, 5, 4, 7, 8] -> [4, 4, 8] return 3
-   size koodai_commonof(koodai a, koodai b, koodai c)
-   really common matching items but we will call it common items for now
-   return size of koodai c i.e. nc = na - nb
- */
-int koodai_commonitems(int a[], int alim, int na,
-		       int b[], int blim, int nb,
-		       int c[], int clim);
-// return size of koodai c i.e. nc = na + nb
-
-/* OPERATION: koodai item sort size min to max
-   [5, 4, 8, 2, 9, 6]         -> [2, 4, 5, 6, 8, 9] 
-   [2, 7, 9, 10, 3, 7, 9, 11] -> [2, 3, 7, 7, 9, 10, 11]
-   koodai_sortup(koodai)
- */
-void koodai_sortdown(int k[], int klim, int nk);
-
-/* OPERATION: koodai item sort size max to min
-   [5, 4, 8, 2, 9, 6]      -> [9, 8, 6, 5, 4, 2] 
-   [2, 7, 9, 10, 3, 7, 9, 11] -> [11, 10, 9, 7, 7, 3, 2]
-   koodai_sordown(koodai)
- */
-void koodai_sortdown(int k[], int klim, int nk);
-
-/* OPERATION: koodai take unique value and it's remove more than one value.
- * self operation i.e. modifies itself 
-  [2, 3, 7, 7, 9, 10, 11] -> [2, 3, 7, 9, 10, 11] return size
-  koodai_unique(int k[100], int n)
-*/
-int koodai_unique(int a[], int alim, int na);
-
-[ 2, 3, 7, 7, 9, 10, 11] -> 7
-int koodai_size(int k[100], int n);
-
-[ 2, 3, 7, 7, 9, 10, 11] -> [] return 0
-int koodai_reset(int k[100], int n);
-koodai_reset()
-koodai_zero()
-koodai_init()
-
-[] -> true
-[1, 3] -> false
-boolean kooda_is_empty(koodai)
-
-[ 2, 3, 7, 7, 7, 9, 9, 10, 11] -> [7, 9]
-void koodai_duplicate_item_values(int k[100], int n, int dk[100]);
-
-[ 2, 3, 7, 7, 7, 9, 9, 10, 11] -> [7, 7, 7, 9, 9]
-void koodai_duplicate_items(int k[100], int n, int dk[100]);
-
-[ 2, 3, 7, 7, 9, 10, 11] check for 3 -> true
-[ 2, 3, 7, 7, 9, 10, 11] check for 12 -> false
-int koodai_isitempresent(int k[100], int n, int val);
-
-[ 2, 3, 7, 7, 9, 10, 11] -> 2 3 7 7 9 10 11
-void koodai_print(int k[100], int n);
-
-int koodai_max(int k[100], int n);
-int koodai_min(int k[100], int n);
-
-if (a == b)
-a[2, 3, 4, 5] == b[2, 3, 4, 5] -> true 
-a[2, 3, 4, 5] == b[4, 2, 3, 5] -> true 
-a[1, 1]       == b[1]          -> false
-koodai_is_samam(a, b)
-
-[2, 3] any overlap with [3, 4] -> true because overlap is 3
-[2, 3] any overlap with [4, 5] -> false because no overlap
-koodai_any_overlap_with(a, b)
-
-if (a > b)
-[2, 3, 4, 5] is_perisu [2, 3, 4] -> true
-[2, 3, 4, 5] is_perisu [2, 3, 4, 5] -> false because they are equal 
-[2, 3, 4, 5, 5] is_perisu [2, 3, 4, 5] -> true because of second 5
-bool koodai_is_perisu(a, b)
-
-if (a < b)
-[2, 3] is_chinnadhu [2, 3, 4] -> true
-[2, 3, 4, 5] is_chinnadu [2, 3, 4, 5] -> false because they are equal
-[2, 2] is_chinnadhu [2, 3, 4, 5] -> false because of second 2
-[2, 2] is_chinnadhu [2, 2, 4, 5] -> true
-bool kooda_is_chinnadhu()
-
-can koodai b be put into koodai a i.e.
-a. is koodai a extensible for contents of b or
-b. does koodai have enough space for contents of b
-bool koodai_canfit(a, b)
-
-addto
-add contents of koodai b into kooda a 
-a += b
-b = 0
-size koodai_roppidu(a, b) i.e. koodai_add_also_koodai(a, b)
-koodai_gaali_pannidu(b) i.e. koodai_reset(b)
+int koodai_add_item(int k[], int kcap, int kn, int value); // API DOIT
