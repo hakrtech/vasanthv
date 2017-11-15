681bfddccb19a9413bcc247d03396338d4505065
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2017-11-15 15:00:38 +0530

    #4 koodai.h

--- kochan/c08/px/koodai.h
+++ kochan/c08/px/koodai.h
@@ -1,42 +1,99 @@
-// oru kooda sunlight
+/* koodai library - unordered, non unique items in an array */
 
--> []
-koodai_init()
-// a [100] but n = 0
-void koodai_init(int k[100], int n); // cproto
+#define ASIZ (100)
 
-[] + 6 -> [6]
-koodai_add_elem(6)
-void koodai_add_element(int k[100], int n, int value); // cproto
+/* OPERATION: initialise koodai
+   -> [] return 0 (size)
+   size koodai_init(koodai) 
+*/
+int koodai_init(int k[], int klim, int nk); // API DOIT
+/* return 0 i.e the size of the new koodai 
+ */
 
-[6, 7, 8] - 6 if present -> [7, 8]
-[6, 7, 8] - 9 if present -> [6, 7, 8]
-koodai_remove_elem(6)
-int koodai_remove_element(int k[100], int n, int value);
-// if removed returned n - 1
-// if !removed returned n
+/* NOTES: a koodai is implemented by an array so 
+ * we need array values, size of the array, current size of the koodai
+ * (k[], klim, ksiz) defines a koodai
+ */
 
-6 7 8 E // n = 3
-6 8 -1 // n = 2 
-E = -1
+/* OPERATION: add item to koodai
+   [] + 6 -> [6] return 1 i.e. size
+   [2, 3] + 7 -> [2, 3, 7] return 3
+   size koodai_add_item(koodai , item) 
+*/
+int koodai_add_item(int k[], int klim, int nk, int value); // API DOIT
+
+/* OPERATION: remove item from koodai
+   [6, 7, 8]    remove 7 if present -> [6, 8]    return 2
+   [6, 7, 7, 8] remove 7 if present -> [6, 7, 8] return 3
+   [6, 7, 8]    remove 9 if present -> [6, 7, 8] return 3
+   size koodai_remove_item(koodai, item) 
+ */
+int koodai_remove_item(int k[], int klim, int nk, int value); // API DOIT
+/* return n-1 if element removed 
+   return n   if element !removed
+ */
+
+/* OPERATION: add two koodais 
+   c = a + b
+   a[2, 7, 9, 10] + b[3, 7, 9, 11] -> c[2, 7, 9, 10, 3, 7, 9, 11] return 8
+   a[1, 1, 2, 12] + b[2, 2, 9, 12] -> c[1, 1, 2, 11, 2, 2, 9, 12] return 8
+   size koodai_add(koodai a, koodai koodai b, koodai c)
+ */
+int koodai_add( int a[], int alim, int na,		// API DOIT
+		int b[], int blim, int nb,		// API DOIT
+		int c[], int clim);			// API DOIT
+
+/* OPERATION: subtract two koodais i.e. remove matching items of 2nd koodai from first koodai and store in third koodai
+   c = a - b
+   [2, 7, 9, 10]    minus [3, 7, 9, 11] ->  [2, 10]    return 2
+   [2, 7, 7, 9, 10] minus [3, 7, 9, 11] ->  [2, 7, 10] return 3
+   size koodai_minus(kodai a, kodai b, kodai c)
+*/
+int koodai_subtract_koodai(int a[], int alim, int na,
+		  	 int b[], int blim, int nb,
+		  	 int c[], int clim);
+/* return size of koodai c i.e. nc = na + nb
+ */
 
 
-[2, 7, 9, 10] U [ 3, 7, 9, 11 ] -> [2, 7, 9, 10, 3, 7, 9, 11] // plus
-c = a + b
-void koodai_union(int ak[100], int bk[100], int n, int ck[100], int m);
+/* OPERATION: commonitems items of two koodais
+   [2, 7, 9, 10]    commonitems [3, 7, 9, 11]   -> [7, 9] return 2
+   [4, 8, 4, 6, 2]  commonitems [4, 5, 6, 7, 8] -> [ 
 
- k, m, c  l       ms mo cp ec
-[2, 7, 9, 10] minus [ 3, 7, 9, 11 ] ->  [ 2, 10 ]// minus
-[2, 7, 7, 9, 10] minus [ 3, 7, 9, 11 ] ->  [ 2, 7, 10 ]// minus
-void koodai_minus(int ak[100], int bk[100], int n, int ck[100]);
+   A[4, 8, 4, 6, 2] 
+ = A[2, 4, 4, 6, 8] 
+   B[4, 5, 6, 7, 8]
+
+   A[2, 4, 4,    6,    8] 
+   B[   4,    5, 6, 7, 8]
+ A.B[   4,       6,    8]
+
+   [4, 8, 4, 6, 2] commonitems [4, 5, 4, 7, 8] -> [4, 4, 8] return 3
+   size koodai_commonof(koodai a, koodai b, koodai c)
+   really common matching items but we will call it common items for now
+ */
+int koodai_commonitems(int a[], int alim, int na,
+		       int b[], int blim, int nb,
+		       int c[], int clim);
+
+/* OPERATION: koodai item sort size min to max
+   [5, 4, 8, 2, 9, 6]      -> [2, 4, 5, 6, 8, 9] 
+   [2, 7, 9, 10, 3, 7, 9, 11] -> [2, 3, 7, 7, 9, 10, 11]
+   koodai_sortup(koodai)
+ */
+void koodai_sortdown(int k[], int klim, int nk);
+
+/* OPERATION: koodai item sort size max to min
+   [5, 4, 8, 2, 9, 6]      -> [9, 8, 6, 5, 4, 2] 
+   [2, 7, 9, 10, 3, 7, 9, 11] -> [11, 10, 9, 7, 7, 3, 2]
+   koodai_sordown(koodai)
+ */
+void koodai_sortdown(int k[], int klim, int nk);
 
 
-[2, 7, 9, 10] commonitems [ 3, 7, 9, 11 ] ->  [ 7, 9 ]
-void koodai_commonof(int ak[100], int bk[100], int n, int ck[100]);
 
-[2, 7, 9, 10, 3, 7, 9, 11] -> [ 2, 3, 7, 7, 9, 10, 11]
-void koodai_sortup(int k[100], int n);
-void koodai_sortdown(int k[100], int n);
+
+
 
 [ 2, 3, 7, 7, 9, 10, 11] -> [2, 3, 7, 9, 10, 11]
 void koodai_make_unique(int k[100], int n);
@@ -49,6 +106,10 @@ void koodai_empty(int k[100], int n);
 koodai_reset()
 koodai_zero()
 koodai_init()
+
+[] -> true
+[1, 3] -> false
+boolean kooda_is_empty(koodai)
 
 [ 2, 3, 7, 7, 7, 9, 9, 10, 11] -> [7, 9]
 void koodai_dupitems(int k[100], int n, int dk[100]);
@@ -79,8 +140,3 @@ abstract data type  // interface // api
 implement using array 
 [ 1 next ] -> 2 -> 
 
-
-struct koodai {
-	int v[100];
-	int n;
-};
