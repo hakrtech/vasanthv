a3d2530b7e753b5242a75fd04ccd64c06e076bae
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2017-11-15 15:42:35 +0530

    #4 koodai.h

--- kochan/c08/px/koodai.h
+++ kochan/c08/px/koodai.h
@@ -37,6 +37,7 @@ int koodai_remove_item(int k[], int klim, int nk, int value); // API DOIT
    c = a + b
    a[2, 7, 9, 10] + b[3, 7, 9, 11] -> c[2, 7, 9, 10, 3, 7, 9, 11] return 8
    a[1, 1, 2, 12] + b[2, 2, 9, 12] -> c[1, 1, 2, 11, 2, 2, 9, 12] return 8
+   return size of koodai c i.e. nc = na + nb
    size koodai_add(koodai a, koodai koodai b, koodai c)
  */
 int koodai_add( int a[], int alim, int na,		// API DOIT
@@ -47,6 +48,7 @@ int koodai_add( int a[], int alim, int na,		// API DOIT
    c = a - b
    [2, 7, 9, 10]    minus [3, 7, 9, 11] ->  [2, 10]    return 2
    [2, 7, 7, 9, 10] minus [3, 7, 9, 11] ->  [2, 7, 10] return 3
+   return size of koodai c i.e. nc = na - nb
    size koodai_minus(kodai a, kodai b, kodai c)
 */
 int koodai_subtract_koodai(int a[], int alim, int na,
@@ -71,13 +73,15 @@ int koodai_subtract_koodai(int a[], int alim, int na,
    [4, 8, 4, 6, 2] commonitems [4, 5, 4, 7, 8] -> [4, 4, 8] return 3
    size koodai_commonof(koodai a, koodai b, koodai c)
    really common matching items but we will call it common items for now
+   return size of koodai c i.e. nc = na - nb
  */
 int koodai_commonitems(int a[], int alim, int na,
 		       int b[], int blim, int nb,
 		       int c[], int clim);
+// return size of koodai c i.e. nc = na + nb
 
 /* OPERATION: koodai item sort size min to max
-   [5, 4, 8, 2, 9, 6]      -> [2, 4, 5, 6, 8, 9] 
+   [5, 4, 8, 2, 9, 6]         -> [2, 4, 5, 6, 8, 9] 
    [2, 7, 9, 10, 3, 7, 9, 11] -> [2, 3, 7, 7, 9, 10, 11]
    koodai_sortup(koodai)
  */
@@ -93,16 +97,18 @@ void koodai_sortdown(int k[], int klim, int nk);
 
 
 
-
-
-[ 2, 3, 7, 7, 9, 10, 11] -> [2, 3, 7, 9, 10, 11]
-void koodai_make_unique(int k[100], int n);
+/* OPERATION: koodai take unique value and it's remove more than one value.
+ * self operation i.e. modifies itself 
+  [2, 3, 7, 7, 9, 10, 11] -> [2, 3, 7, 9, 10, 11] return size
+  koodai_unique(int k[100], int n)
+*/
+int koodai_unique(int a[], int alim, int na);
 
 [ 2, 3, 7, 7, 9, 10, 11] -> 7
 int koodai_size(int k[100], int n);
 
-[ 2, 3, 7, 7, 9, 10, 11] -> []
-void koodai_empty(int k[100], int n);
+[ 2, 3, 7, 7, 9, 10, 11] -> [] return 0
+int koodai_reset(int k[100], int n);
 koodai_reset()
 koodai_zero()
 koodai_init()
@@ -112,7 +118,10 @@ koodai_init()
 boolean kooda_is_empty(koodai)
 
 [ 2, 3, 7, 7, 7, 9, 9, 10, 11] -> [7, 9]
-void koodai_dupitems(int k[100], int n, int dk[100]);
+void koodai_duplicate_item_values(int k[100], int n, int dk[100]);
+
+[ 2, 3, 7, 7, 7, 9, 9, 10, 11] -> [7, 7, 7, 9, 9]
+void koodai_duplicate_items(int k[100], int n, int dk[100]);
 
 [ 2, 3, 7, 7, 9, 10, 11] check for 3 -> true
 [ 2, 3, 7, 7, 9, 10, 11] check for 12 -> false
