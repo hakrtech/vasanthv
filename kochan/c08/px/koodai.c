ffcfaa0ab2a239d3ce4f7128ca9195ca644b060f
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2017-11-16 16:48:03 +0530

    #4 koodai.c checking

--- kochan/c08/px/koodai.c
+++ kochan/c08/px/koodai.c
@@ -106,6 +106,108 @@ int koodai_add_item(int k[], int kcap, int kn, int value) // API DONE
 	return kn;
 }
 
+
+	/* --- next level --- */
+
+/* OPERATION: remove item from koodai
+   [6, 7, 8]    remove 7 if present -> [6, 8]    return 2
+   [6, 7, 7, 8] remove 7 if present -> [6, 7, 8] return 3
+   [6, 7, 8]    remove 9 if present -> [6, 7, 8] return 3
+   size koodai_remove_item(koodai, item) 
+   return n-1 if element removed 
+   return n   if element !removed
+ */
+int koodai_remove_item(int k[], int kcap, int kn, int value) // API DOIT
+{
+	return 1;
+}
+
+/* OPERATION: add two koodais 
+   c = a + b
+   a[2, 7, 9, 10] + b[3, 7, 9, 11] -> c[2, 7, 9, 10, 3, 7, 9, 11] return 8
+   a[1, 1, 2, 12] + b[2, 2, 9, 12] -> c[1, 1, 2, 11, 2, 2, 9, 12] return 8
+   return size of koodai c i.e. nc = na + nb
+   size koodai_add(koodai a, koodai b, koodai c)
+   return n+1 if element removed 
+   return n   if element !removed
+ */
+int koodai_add(int a[], int acap, int an,
+	       int b[], int bcap, int bn,		
+	       int c[], int ccap)	// API DOIT
+{
+	return 1;
+}
+
+/* OPERATION: subtract two koodais i.e. remove matching items of 2nd koodai from first koodai and store in third koodai
+   c = a - b
+   [2, 7, 9, 10]    minus [3, 7, 9, 11] ->  [2, 10]    return 2
+   [2, 7, 7, 9, 10] minus [3, 7, 9, 11] ->  [2, 7, 10] return 3
+   return size of koodai c i.e. nc = na - nb
+   size koodai_minus(kodai a, kodai b, kodai c)
+*/
+int koodai_subtract_koodai(int a[], int acap, int an,
+		  	   int b[], int bcap, int bn,
+		  	   int c[], int ccap)	// API DOIT
+{
+	return 1;
+}
+
+/* OPERATION: commonitems items of two koodais
+   [2, 7, 9, 10]    commonitems [3, 7, 9, 11]   -> [7, 9] return 2
+   [4, 8, 4, 6, 2]  commonitems [4, 5, 6, 7, 8] -> [ 
+
+   A[2, 4, 4, 6, 8] 
+   B[4, 5, 6, 7, 8]
+
+   A[2, 4, 4,    6,    8] 
+   B[   4,    5, 6, 7, 8]
+ A.B[   4,       6,    8]
+
+   [4, 8, 4, 6, 2] commonitems [4, 5, 4, 7, 8] -> [4, 4, 8] return 3
+   size koodai_commonof(koodai a, koodai b, koodai c)
+   really common matching items but we will call it common items for now
+   return size of koodai c i.e. nc = na - nb
+ */
+int koodai_commonitems(int a[], int acap, int an,
+		       int b[], int bcap, int bn,
+		       int c[], int ccap)	// API DOIT
+{
+	return 1;
+}
+
+/* OPERATION: koodai item sort size min to max
+   [5, 4, 8, 2, 9, 6]         -> [2, 4, 5, 6, 8, 9] 
+   [2, 7, 9, 10, 3, 7, 9, 11] -> [2, 3, 7, 7, 9, 10, 11]
+   koodai_sortup(koodai)
+ */
+void koodai_sortup(int k[], int kcap, int kn)	// API DOIT
+{
+	return;
+}
+
+/* OPERATION: koodai item sort size max to min
+   [5, 4, 8, 2, 9, 6]      -> [9, 8, 6, 5, 4, 2] 
+   [2, 7, 9, 10, 3, 7, 9, 11] -> [11, 10, 9, 7, 7, 3, 2]
+   koodai_sordown(koodai)
+ */
+void koodai_sortdown(int k[], int kcap, int kn) // API DOIT
+{
+	return;
+}
+
+/* OPERATION: koodai take unique value and it's remove more than one value.
+   self operation i.e. modifies itself 
+   [2, 3, 7, 7, 9, 10, 11] -> [2, 3, 7, 9, 10, 11] return size
+   return n-1 if element removed 
+   return n   if element !removed
+   koodai_unique(int k[100], int n)
+*/
+int koodai_unique(int k[], int kcap, int kn) // API DOIT
+{
+	return 1;
+}
+
+
 #ifdef TEST
 int main(void)
 {
@@ -148,3 +250,4 @@ int main(void)
 	return 0;
 }
 #endif // TEST
+
