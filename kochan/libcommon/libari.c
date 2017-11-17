4a47202111414cbd1c7ac3f388d78242fd0a7e0a
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2017-11-17 17:57:20 +0530

    #4 libarray functions added

--- kochan/libcommon/libari.c
+++ kochan/libcommon/libari.c
@@ -9,15 +9,9 @@
 
 void ari_setall(int a[], int n, int val) // OKR
 {
-	int i;
-
 	assert(n > 0);
 
-	for (i = 0; i < n; i++) {
-		a[i] = val;
-	}
-
-	return;
+	ari_range_set(a, n, 0, n-1, val);
 }
 
 void ari_setone(int a[], int n, int pos, int val) // OKR
@@ -26,12 +20,10 @@ void ari_setone(int a[], int n, int pos, int val) // OKR
 	assert((0 <= pos) && (pos < n));
 
 	a[pos] = val;
-
-	return;
 }
 
 // a[rstart..rend] = val 
-void ari_range_set(int a[], int n, int rstart, int rend, int val)
+void ari_range_set(int a[], int n, int rstart, int rend, int val) // OKR
 {
 	int i;
 
@@ -40,73 +32,160 @@ void ari_range_set(int a[], int n, int rstart, int rend, int val)
 	assert(rend < n);
 
 	for (i = rstart; i <= rend; ++i) {
-		a[i] = val;
+		ari_setone(a, n, i, val);
 	}
-
-	return;
 }
 
 void ari_print(int a[], int n) // OKR
 {
+	assert(n > 0);
+
+	ari_range_print(a, n, 0, n-1);
+}
+
+// print a[rstart..rend]
+void ari_range_print(int a[], int n, int rstart, int rend) // OKR
+{
 	int i;
 
 	assert(n > 0);
-	
-	for (i = 0; i < n; i++) {
+	assert((0 <= rstart) && (rstart <= rend));
+	assert(rend < n);
+
+	for (i = rstart; i <= rend; ++i) {
 		printf("%d ", a[i]);
 	}
 	printf("\n");
-
-	return;
 }
 
-void ari_plusk(int a[], int n, int k) // OKR
+// a[] += k 
+void ari_addk(int a[], int n, int k) // OKR
 {
-	int i = 0;
-
 	assert(n > 0);
 
-	while (i < n) {
+	ari_range_addk(a, n, 0, n-1, k);
+}
+
+// a[] -= k 
+void ari_subk(int a[], int n, int k) // OKR
+{
+	assert(n > 0);
+
+	ari_range_subk(a, n, 0, n-1, k);
+}
+
+// a[] *= k 
+void ari_mulk(int a[], int n, int k) // OKR
+{
+	assert(n > 0);
+
+	ari_range_mulk(a, n, 0, n-1, k);
+}
+
+// a[] /= k for k != 0
+void ari_divk(int a[], int n, int k) // OKR
+{
+	assert(n > 0);
+	assert(k != 0);
+
+	ari_range_divk(a, n, 0, n-1, k);
+}
+
+// similarly for range
+void ari_range_addk(int a[], int n, int rstart, int rend, int k) // OKR
+{
+	int i;
+
+	assert(n > 0);
+	assert((0 <= rstart) && (rstart <= rend));
+	assert(rend < n);
+
+	for (i = rstart; i <= rend; ++i) {
 		a[i] = a[i] + k;
-		++i;
 	}
 }
 
-int ari_sum(int a[], int n) // OKR
+void ari_range_subk(int a[], int n, int rstart, int rend, int k) // OKR
+{
+	int i;
+	
+	assert(n > 0);
+	assert((0 <= rstart) && (rstart <= rend));
+	assert(rend < n);
+
+	for (i = rstart; i <= rend; ++i) {
+		a[i] = a[i] - k;
+	}
+}
+
+void ari_range_mulk(int a[], int n, int rstart, int rend, int k) // OKR
+{
+	int i;
+
+	assert(n > 0);
+	assert((0 <= rstart) && (rstart <= rend));
+	assert(rend < n);
+
+	for (i = rstart; i <= rend; ++i) {
+		a[i] = a[i] * k;
+	}
+}
+
+void ari_range_divk(int a[], int n, int rstart, int rend, int k) // OKR
+{
+	int i;
+
+	assert(n > 0);
+	assert(k != 0);
+	assert((0 <= rstart) && (rstart <= rend));
+	assert(rend < n);
+
+	for (i = rstart; i <= rend; ++i) {
+		a[i] = a[i] / k;
+	}
+}
+
+int ari_sum(int a[], int n)
+{
+	int sum = 0;
+
+	assert(n > 0);
+
+	sum = ari_range_sum(a, n, 0, n-1);
+
+	return sum;
+}
+
+int  ari_range_sum(int a[], int n, int rstart, int rend)
 {
 	int i;
 	int sum = 0;
 
 	assert(n > 0);
+	assert((0 <= rstart) && (rstart <= rend));
+	assert(rend < n);
 
-	for (i = 0; i < n; ++i) {
+	for (i = rstart; i <= rend; ++i) {
 		sum += a[i];
 	}
 
 	return sum;
 }
 
+// return maximum of a[]
+int  ari_getmax(int a[], int n);
+// return first leftmost pos of maximum
+// return -1 if not found
+int  ari_getmaxpos(int a[], int n, int pos);
 
-#if 0
+// similarly for minimum
+int  ari_getmin(int a[], int n);
+int  ari_getminpos(int a[], int n, int val);
 
-// a[0..n-1] = random number from randbot to randtop both included
-void ari_setall_random(int a[], int n, int randbot, int randtop);
-// a[rstart..rend] = random number from randbot to randtop both included
-void ari_range_set_random(int a[], int n, int rstart, int rend, int randbot, int randtop);
-
-// print a[rstart..rend]
-void ari_range_print(int a[], int n, int rstart, int rend);
-// (5,3,4,9) leading=(, trailing=), separator=,
-// (5 3 4 9) leading=(, trailing=), separator=' '
-// [5 3 4 9] leading=[, trailing=], separator=' '
-void ari_print_format(int a[], int n, char leading, char trailing, char separator);
-// print a[rstart..rend] with format characters
-void ari_range_print_format(int a[], int n, int rstart, int rend, char leading, char trailing, char separator);
-
-// sum = sum of a[rstart..rend]
-int  ari_range_sum(int a[], int n, int rstart, int rend);
-#endif
-
+// true if all values are equal across a[] and b[]
+bool ari_isequal(int a[], int na, int b[], int nb);
+bool ari_range_isequal(int a[], int na, int rstarta, int renda, 
+		       int b[], int nb, int rstartb, int rendb);
 
 bool ari_isvalpresent(int a[], int n, int val) // OKR
 {
@@ -220,4 +299,5 @@ int ari_factor(int a[], int n)
 
 	return count;
 }
+
 
