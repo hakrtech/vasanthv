b447cf83464a7f27bcad20cc40f89c3ad3e3c17d
Dinesh <dinesh.thirumurthy@gmail.com>
2017-12-04 19:11:43 +0530

    #4 apistat.sh updated

--- kochan/libcommon/apistat.sh
+++ kochan/libcommon/apistat.sh
@@ -2,28 +2,28 @@
 LIB=ari
 HSRC=lib${LIB}.h
 CSRC=lib${LIB}.c
-HOUT=$LIB.h.spec.txt	# header api
-COUT=$LIB.c.spec.txt	# c implementation
-rm -f $HOUT $COUT
+H=$LIB.h.spec.txt	# header api
+C=$LIB.c.spec.txt	# c implementation
+rm -f $H $C
 
 # echo -n "$HSRC	"
-# cat $HOUT | egrep '^void|^int|^bool' | cat -n 
+# cat $H | egrep '^void|^int|^bool' | cat -n 
 
-cat $HSRC | egrep '^void|^int|^bool|^double' | awk -F\; '{ print $1; }' | sed -e 's/  [ ]*/ /g' | sed -e 's/)//' | sort -u > $HOUT
+cat $HSRC | egrep '^void|^int|^bool|^double' | awk -F\; '{ print $1; }' | sed -e 's/  [ ]*/ /g' | sed -e 's/)//' | sort -u > $H
 
-echo "$COUT	"
-# cat $COUT | egrep '^void|^int|^bool|^double' | grep OKR | cat -n
+echo "$C	"
+# cat $C | egrep '^void|^int|^bool|^double' | grep OKR | cat -n
 
-cat $CSRC | egrep '^void|^int|^bool|^double' | grep OKR | awk -F\) '{ print $1; }' | sed -e 's/  [ ]*/ /g' | sort -u > $COUT
+cat $CSRC | egrep '^void|^int|^bool|^double' | grep OKR | awk -F\) '{ print $1; }' | sed -e 's/  [ ]*/ /g' | sort -u > $C
 
-echo "$HSRC Array API - TODO"
-diff $HOUT $COUT | egrep '^<' | egrep -v '_range_|_stat_' | sed -e 's/^< //' | cat -n
+# echo "$HSRC Array API - TODO"
+diff $H $C | egrep '^<' | grep ari_ | egrep -v '_range_|_stat_' | sed -e 's/^< //' | cat -n
 
 echo "$HSRC Array Range API - TODO"
-diff $HOUT $COUT | egrep '^<' | grep _range_ | sed -e 's/^< //' | cat -n
+diff $H $C | egrep '^<' | grep ari_ | grep _range_ | sed -e 's/^< //' | cat -n
 
-echo "$HSRC Statistics API - TODO"
-diff $HOUT $COUT | egrep '^<' | grep _stat_ | sed -e 's/^< //' | cat -n
+# echo "$HSRC Statistics API - TODO"
+# diff $H $C | egrep '^<' | grep ari_ | grep _stat_ | sed -e 's/^< //' | cat -n
 
-echo "$HSRC Today Targets - TODO"
-grep TODO $HSRC
+# echo "$HSRC Today Targets - TODO"
+# grep TODO $HSRC
