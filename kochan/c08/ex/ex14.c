4e56035dbbbe4d92026eb4d4af22b282222a503c
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2018-01-04 17:45:47 +0530

    #4 ex14 completed

--- kochan/c08/ex/ex14.c
+++ kochan/c08/ex/ex14.c
@@ -6,11 +6,20 @@
 
 #include "libinteger.h"
 #include "libarray.h"
+#include "libmatrix.h"
 
 int num;
 _Bool isprime = 0;
 int a[10];
 int n;
+int m[100][100] = { 
+	{ 1,  2,  3,  4,  5},
+	{ 6,  7,  8,  9, 10},
+	{11, 12, 13, 14, 15},
+	{16, 17, 18, 19, 20}
+ };
+int tpos_n[100][100];
+int r, c;
 
 void prime(void)
 {
@@ -30,14 +39,53 @@ void arraysum(void)
 	printf("sum of array %d\n", num);
 }
 
+void tposmatrix(void)
+{
+	printf("M[4*5] matrix>\n");
+	mxi_print(m, r, c);
+	printf("N[5*4] transposeMatrix>\n");
+	mxi_transpose(m, r, c, tpos_n);
+	mxi_print(tpos_n, c, r);
+}
+
+void sort(void)
+{
+	ari_range_set_square(a, n, 3, 6);
+	printf("Choice:\n 1. ascending order\n 2. descending order\n 3. print array\t > ");
+	scanf("%d", &num);
+	printf("%d\n", num);
+
+	switch (num) {
+	case 1:
+		printf("sorted ascending order\n");
+		ari_sortup(a, n);
+		ari_print(a, n, "a[asc]");
+		break;
+	case 2:
+		printf("sorted descenting order\n");
+		ari_sortdown(a, n);
+		ari_print(a, n, "a[des]");
+		break;
+	case 3:
+		printf("printed array\n");
+		ari_print(a, n, "a[]");
+		break;
+	default:
+		printf("choice incorrect!!!\n");
+	}
+}
+
 int main(void)
 {
 	num = 2;
 	n = 10;
-
 	prime();
 	arraysum();
 	prime();
+	r = 4;
+	c = 5;
+	tposmatrix();
+	sort();
 
 	return 0;
 }
