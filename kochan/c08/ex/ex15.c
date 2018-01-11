412a7180b2c808807ee7f146bec3167411039eac
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2018-01-11 20:23:43 +0530

    #4 c08 ex15 inputs function

--- kochan/c08/ex/ex15.c
+++ kochan/c08/ex/ex15.c
@@ -5,23 +5,24 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #include "libbase.h"
+#include "libarray.h"
 
 int input_decimal_int(char *prompt)
 {
 	int val;
 	int d;
 
-	printf("%s: ", prompt);
-
+	printf("%s", prompt);
 	d = scanf("%d", &val);
 	if (d != 1) {
 		printf("scanf error %d\n", d);
 		exit(1);
 	}
 
-	printf(" %d\n", val);
+	//printf(" %d\n", val);
 
 	return val;
 }
@@ -31,14 +32,45 @@ int input_decimal_int_until_within_range(int minrange, int maxrange, char *promp
 	int val;
 
 	do {
-		printf("%s(%d..%d)", prompt, minrange, maxrange);
+		printf("%s(%d..%d): ", prompt, minrange, maxrange);
 		val = input_decimal_int("");
 	} while ((val < minrange) || (val > maxrange));
 
 	return val;
 }
 
-// int input_decimal_ints(int a[], int asize, int ninputs);
+int input_decimal_ints(int a[], int asize, int ninputs)
+{
+	int i;
+	int left, right;
+	int start, stop;
+	bool debug = false;
+
+	assert(asize > 0);
+	assert((0 < ninputs) && (ninputs <= asize));
+
+	// define range
+	left = 0;
+	right = asize - 1;
+
+	// define loop a[0 .. ninputs-1]
+	start = 0;
+	stop = ninputs - 1;
+
+	for (i = start; i <= stop; i++) {
+		assert((left <= i) && (i <= right));
+
+		a[i] = input_decimal_int("enter input: ");
+	}
+
+	if (debug) {
+		ari_print(a, asize, "chk");
+		printf("%d\n", i);
+	}
+
+	return i;
+}
+
 // int input_decimal_ints_until_exitval(int a[], int asize);
 
 int main(void)
@@ -46,20 +78,24 @@ int main(void)
 	int num;
 	int base;
 #define MAX 100
+	int inputs[MAX];
 	char a[MAX];
 	int n;
 
 	printf("convert base of value >\n");
 
-	num = input_decimal_int("enter num");
+	num = input_decimal_int("enter num: ");
 	base = input_decimal_int_until_within_range(2, 36, "enter base");
 
 	arc_setall(a, MAX, '-');
 	n = baseconv_base10_to_basen(num, a, MAX, base);
 
-	printf("num %d\n", num);
-	printf("base %d\n", base);
+	printf("num %d\nbase %d\n", num, base);
 	arc_base_print(a, n, "values");
+
+	ari_setall(inputs, MAX, -1);
+	n = input_decimal_ints(inputs, MAX, 5);
+	ari_print(inputs, n, "inputs");
 
 	return 0;
 }
