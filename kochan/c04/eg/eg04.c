881bb8d389d8bde853ae3e431334358b28d8d5e6
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2018-01-21 10:04:36 +0000

    #28 c04 eg04

---
+++ kochan/c04/eg/eg04.c
@@ -0,0 +1,18 @@
+/* chapter 04 example 04 the modulus operator
+ * vasanth 21 january 2018
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 25, b = 5, c = 10, d = 7;
+
+	printf("a %% b = %i\n", a % b);
+	printf("a %% c = %i\n", a % c);
+	printf("a %% d = %i\n", a % d);
+	printf("a / d * d + a %% d = %i\n", a / d * d + a % d);
+
+	return 0;
+}
+
