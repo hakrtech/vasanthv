c0f761f74b19909f19a561b8706342acee034ed6
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2018-01-22 06:52:38 +0000

    #28 c04 ex05

---
+++ kochan/c04/ex/ex05.c
@@ -0,0 +1,17 @@
+/* chapter 04 exercise 05 expect output
+ * vasanth 22 january 2018
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char c, d;
+
+	c = 'd';
+	d = c;
+	printf("d = %c\n", d);
+
+	return 0;
+}
+
