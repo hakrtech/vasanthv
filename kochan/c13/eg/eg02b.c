73a96a2b1a6219b6e957132b304606511299bddc
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2018-01-26 16:32:44 +0000

    #9 c13 eg02b uppercase convertion function

---
+++ kochan/c13/eg/eg02b.c
@@ -0,0 +1,31 @@
+/* chapter 13 example 02b program to convert an ascii character from lowercase to uppercase
+ * vasanth 26 january 2018
+ */
+
+#include <stdio.h>
+
+#define IS_LOWER_CASE(x) (((x) >= 'a') && ((x) <= 'z'))
+#define TO_UPPER(x) (IS_LOWER_CASE(x) ? (x) - 'a' + 'A' : (x))
+
+void uppercase(char *s)
+{
+	while (*s != '\0') {
+		*s = TO_UPPER(*s);
+		++s;
+	}
+}
+
+int main(void)
+{
+	char str[100];
+
+	printf("conversion of characters lowercase to upppercase>\n");
+	printf("enter word: ");
+	scanf("%s", str);
+
+	uppercase(str);
+	printf("uppercase: %s\n", str);	
+
+	return 0;
+}
+
