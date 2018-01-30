b0a790191a5edc9d652cdeea9e00851251a7b02c
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2018-01-30 11:32:51 +0000

    #9 c13 ex03 modified

--- kochan/c13/ex/ex03.c
+++ kochan/c13/ex/ex03.c
@@ -5,17 +5,26 @@
 #include <stdio.h>
 
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#define MIN_THREE(a, b, c) (((MIN(a, b)) < (c)) ? (MIN(a, b)) : (c))
+#define MIN_FOUR(a, b, c, d) ((MIN_THREE(a, b, c) < (d)) ? (MIN_THREE(a, b, c)) : (d))
 
 int main(void)
 {
-	int a, b;
+	int a, b, c, d;
 
 	printf("minimum of two values>\n");
 	printf("enter val a = ");
 	scanf("%d", &a);
 	printf("enter val b = ");
 	scanf("%d", &b);
+	printf("enter val c = ");
+	scanf("%d", &c);
+	printf("enter val d = ");
+	scanf("%d", &d);
+
 	printf("a = %d b = %d\nminimum = %d\n", a, b, MIN(a, b));
+	printf("a = %d b = %d c = %d\nminimum = %d\n", a, b, c, MIN_THREE(a, b, c));
+	printf("a = %d b = %d c = %d d = %d\nminimum = %d\n", a, b, c, d, MIN_FOUR(a, b, c, d));
 
 	return 0;
 }
