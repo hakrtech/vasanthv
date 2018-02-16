016581dd9f1541783048f341d06ba7884b6b8564
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2018-02-16 15:35:23 +0530

    #5 c09 eg Makefile and loc

--- kochan/loc.sh
+++ kochan/loc.sh
@@ -9,4 +9,16 @@ echo "Target LOC:      $TLOC"
 PCT=$(echo "$LOC*100/$TLOC" | bc)
 CPCT=$(echo "$CLOC*100/$TLOC" | bc)
 echo "Target Achieved: $PCT% ($CPCT%)"
+for d in `ls -1d c* lib*`
+do
+	LOC=$(find $d -name "*.[c]" -print | xargs wc -l | tail -1 | awk '{ print $1; }')
+	CLOC=$(find $d -type f -name "*.c" -print | xargs cat | sed -e '/^[ 	]*$/d' | awk -f rmc.awk | wc -l)
+	echo "Chapter $d $LOC $CLOC"
+done  | awk '
+BEGIN { sum = 0; asum = 0 }
+/./ { sum = sum + $3;
+      asum = asum + $4;
+      printf "%5s %10s visual ... %5d %5d matter ... %5d %5d\n", $1, $2, $3, sum , $4, asum
+    }
+'
 exit 0
