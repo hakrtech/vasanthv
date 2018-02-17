9c8c1c86eb7e360339ddb65899fd9257f622bb88
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2018-02-17 14:09:16 +0530

    #5 c09 eg02 modified

--- kochan/c09/eg/eg02.c
+++ kochan/c09/eg/eg02.c
@@ -9,40 +9,42 @@ int main(void)
 {
 	struct date
 	{
-		int month;
-		int day;
-		int year;
+		int dd;
+		int mm;
+		int yyyy;
 	};
 
 	struct date today, tomorrow;
 	int d;
-	int m[12] = { 31, 28, 31, 30, 31, 30,
-		      31, 31, 30, 31, 30, 31
-		    };
+	const int dd_mm[12] = { 31, 28, 31, 30, 31, 30,
+				31, 31, 30, 31, 30, 31
+			      };
 
-	printf("today date mm:dd:yyyy > ");
-	d = scanf("%d%d%d", &today.month, &today.day, &today.year);
+	printf("today date dd:mm:yyyy > ");
+	d = scanf("%d%d%d", &today.dd, &today.mm, &today.yyyy);
 	if (d != 3) {
 		printf("scanf error %d\n", d);
 		exit(1);
 	}
 
-	printf("%d/%d/%d\n", today.month, today.day, today.year);
+	printf("%d/%d/%d\n", today.dd, today.mm, today.yyyy);
 
-	if (today.day != m[today.month-1]) {
-		tomorrow.day = today.day + 1;
-		tomorrow.month = today.month;
-		tomorrow.year = today.year;
-	} else if (today.month == 12) {
-		tomorrow.day = 1;
-		tomorrow.month = 1;
-		tomorrow.year = today.year + 1;
-	} else {
-		tomorrow.day = 1;
-		tomorrow.month = today.month + 1;
-		tomorrow.year = today.year;
-	}
+	if (today.dd != dd_mm[today.mm-1]) {
+		tomorrow.dd = today.dd + 1;
+		tomorrow.mm = today.mm;
+		tomorrow.yyyy = today.yyyy;
+       } else if (today.mm == 12) {
+	       tomorrow.dd = 1;
+	       tomorrow.mm = 1;
+	       tomorrow.yyyy = today.yyyy + 1;
+        } else {
+               tomorrow.dd = 1;
+	       tomorrow.mm = today.mm + 1;
+	       tomorrow.yyyy = today.yyyy;
+        }
 
-	printf("tomorrow date %d/%d/%d\n", tomorrow.month, tomorrow.day, tomorrow.year);
+	printf("tomorrow date %d/%d/%d\n", tomorrow.dd, tomorrow.mm, tomorrow.yyyy);
+
+	return 0;
 }
 
