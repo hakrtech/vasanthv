6244d739c5f6cb0ea941fe0789cd41fabe807731
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2018-03-01 22:12:23 +0530

    #6 c10 ex10 doing and modified

--- kochan/c10/ex/ex10.c
+++ kochan/c10/ex/ex10.c
@@ -3,6 +3,7 @@
  */
 
 #include <stdio.h>
+#include <stdbool.h>
 #include <assert.h>
 
 struct dic
@@ -11,9 +12,55 @@ struct dic
 	char def[100];
 };
 
-void dic_sort(struct dic note[])
+bool string_compare(char a[], char b[])
 {
-	// something
+	int i = 0;
+	bool is_compared = false;
+
+	while ((a[i] == b[i]) && (a[i] != '\0')) {
+		++i;
+	}
+
+	if (a[i] <= b[i]) {
+		is_compared = true;
+	}
+
+	return is_compared;
+}
+
+bool dic_order(struct dic note[], int len, int j)
+{
+	int i = 0;
+	bool is_compared = false, is_ordered = false;
+
+	while (i < len - 1) {
+		is_compared = string_compare(note[j].word, note[i+1].word);
+		if (is_compared) {
+			printf("yes [ %s ] pos %d\n", note[j].word, i);
+			is_ordered = true;
+		} else {
+			printf("change order i %d j %d\n", i, j);
+			is_ordered = false;
+		}
+		++i;
+	}
+
+	return is_ordered;
+}
+
+void dic_sort(struct dic note[], int len)
+{
+	int j = 0;
+	bool is_ordered = false;
+
+	is_ordered = dic_order(note, len, j);
+	if (is_ordered) {
+		printf("ok\n");
+		++j;
+	} else {
+		printf("chk\n");
+		// do something
+	}
 }
 
 void dic_print(struct dic note[], int len)
@@ -28,7 +75,7 @@ void dic_print(struct dic note[], int len)
 		++i;
 	}
 }
-
+/*
 void read_line(struct dic d[], int pos)
 {
 	char c;
@@ -59,20 +106,26 @@ int dic_entry(struct dic note[])
 		printf(" [ %s ]\n", note[i].def);
 
 		++i;
-	} while (i < 10);
+	} while (i < 3);
 
 	return i;
 }
-
+*/
 int main(void)
 {
-	struct dic note[100];
-	int len;
+	struct dic note[100] = { {"anbu", "vasanth bro"},
+				 {"aout", "program output, out"},
+				 {"cat", "animal, living in home"},
+				 {"doc", "animal, living in home and kill cat"}
+				};
+	int len = 4;
 
 	printf("sort dictionary into alphabetic order>\n");
 	printf("develop own dictionary:\n");
-	len = dic_entry(note);
+	//len = dic_entry(note);
+	//dic_print(note, len);
 	dic_print(note, len);
+	dic_sort(note, len);
 
 	return 0;
 }
