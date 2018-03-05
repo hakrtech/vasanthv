95d2def8d3677c773b80efadd8ea99e847d82ec6
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2018-03-05 14:44:24 +0530

    #6 c10 ex11 modified

--- kochan/c10/ex/ex11.c
+++ kochan/c10/ex/ex11.c
@@ -7,38 +7,44 @@
 
 int string_to_integer(char s[])
 {
-	int i, val, r;
+	int i, digit, result;
 
 	i = 0;
 	if (s[i] == '-') {
 		i = 1;
 	}
 
-	r = 0;
+	result = 0;
 	while ((s[i] >= '0') && (s[i] <= '9')) {
-		val = s[i] - '0';
-		r = r * 10 + val;
+		digit = s[i] - '0';	/* ascii character set calculation
+					   eg. let s[ 0 ... '\0'], s[0] = '5'
+					   	   num = '5' - '0'
+						   num =  53 - 48	(char '5' ascii decimal 53, char '0' ascii decimal 48)
+						   num =  5		(char '5' into digit 5) */
+		result = result * 10 + digit;
 		++i;
 	}
 	assert((s[i] < '0') || (s[i] > '9'));
 
 	if (s[0] == '-') {
-		r = -r;
+		result = -result;
 	}
 
-	return r;
+	return result;
 }
 
 int main(void)
 {
-	char c[100];
-	int d;
+	char source[101];
+	int num;
 
-	printf("convert minus sign and string to an integer>\n");
+	printf("convert a string and minus sign to an integer>\n");
 	printf("enter a string: ");
-	scanf("%s", c);
-	d = string_to_integer(c);
-	printf("string [ %s ] integer %d\n", c, d);
+	scanf("%s", source);
+	printf("[ %s ]\n", source);
+
+	num = string_to_integer(source);
+	printf("string [ %s ] integer %d\n", source, num);
 
 	return 0;
 }
