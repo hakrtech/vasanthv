30c6d1e879cade179060ed23b925f86f738c36b8
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2018-03-06 15:47:23 +0530

    #6 c10 ex02 fixed

--- kochan/c10/ex/ex02.c
+++ kochan/c10/ex/ex02.c
@@ -6,20 +6,19 @@
 #include <stdbool.h>
 #include <assert.h>
 
-bool string_equal(const char a[], const char b[])
+bool string_equal(char snt1[], char snt2[])
 {
 	bool is_equal = false;
 	int i = 0;
 
-	while ((a[i] == b[i]) && (a[i] != '\0')) {
+	while ((snt1[i] == snt2[i]) && (snt1[i] != '\0')) {
 		++i;
 	}
-	assert((a[i] != b[i]) || (a[i] == '\0'));
+	assert((snt1[i] != snt2[i]) || (snt1[i] == '\0'));
+	is_equal = true;
 
-	if (a[i] != b[i]) {
+	if (snt1[i] != snt2[i]) {
 		is_equal = false;
-	} else {
-		is_equal = true;
 	}
 
 	return is_equal;
@@ -27,23 +26,23 @@ bool string_equal(const char a[], const char b[])
 
 int main(void)
 {
-	char a[100];
-	char b[100];
+	char s1[101], s2[101];
 	bool is_equal = false;
 
 	printf("determine if two strings equal or not>\n");
 	printf("first string: ");
-	scanf("%s", a);
-	printf("[ %s ]\n", a);
-	printf("second string: ");
-	scanf("%s", b);
-	printf("[ %s ]\n", b);
+	scanf("%s", s1);
+	printf("[ %s ]\n", s1);
 
-	is_equal = string_equal(a, b);
+	printf("second string: ");
+	scanf("%s", s2);
+	printf("[ %s ]\n", s2);
+
+	is_equal = string_equal(s1, s2);
 	if (is_equal) {
-		printf("strings [ %s ] [ %s ] equal %d\n", a, b, is_equal);
+		printf("strings [ %s ] [ %s ] equal %d\n", s1, s2, is_equal);
 	} else {
-		printf("strings [ %s ] [ %s ] not equal %d\n", a, b, is_equal);
+		printf("strings [ %s ] [ %s ] not equal %d\n", s1, s2, is_equal);
 	}
 
 	return 0;
