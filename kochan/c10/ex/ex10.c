600fcf8917c2be98f5bc4b338ea5b6393e696b96
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2018-03-06 12:21:21 +0530

    #6 c10 ex10 fixed

--- kochan/c10/ex/ex10.c
+++ kochan/c10/ex/ex10.c
@@ -27,19 +27,13 @@ int string_length(char s[])
 	return i;
 }
 
-bool string_copy(char source[], char dest[], int n)
+void string_copy(char source[], char dest[], int n)
 {
 	int len, i;
-	bool is_copied = false;
 
 	len = string_length(source);
-	assert(n >= len);
-	arc_setall(dest, n, '-');
-
-#define DEBUG 0
-	if (DEBUG) {
-		arc_print(dest, n, "dest");
-	}
+	assert(n > 0);
+	assert(len <= n);
 
 	// loop source [ 0 ... !'\0'] to copy dest [ 0 .. n-1 ]
 	i = 0;
@@ -50,36 +44,32 @@ bool string_copy(char source[], char dest[], int n)
 	}
 	assert(source[i] == '\0');
 	dest[i] = '\0';
-	is_copied = true;
 
+#define DEBUG 0
 	if (DEBUG) {
+		arc_print(source, len, "source");
 		arc_print(dest, n, "dest");
 	}
-
-	return is_copied;
 }
 
-bool string_swap(char a[], char b[])
+void string_swap(char a[], char b[])
 {
 	int a_len, b_len, c_len;
 	char c[201];
 	int n  = 200;
-	bool is_copied = false, is_swaped = false;
 
-	is_copied = string_copy(a, c, n);
-	if (is_copied) {
-		b_len = string_length(b);
-		is_copied = string_copy(b, a, b_len);
-		if (is_copied) {
-			c_len = string_length(c);
-			is_copied = string_copy(c, b, c_len);
-			is_swaped = true;
-		} else {
-			printf("chk2 string copy\n");
-		}
-	} else {
-		printf("chk1 string copy\n");
-	}
+	arc_setall(c, n, '-');
+	string_copy(a, c, n);
+
+	b_len = string_length(b);
+	string_copy(b, a, b_len);
+	a_len = string_length(a);
+	assert(a_len == b_len);
+
+	c_len = string_length(c);
+	string_copy(c, b, c_len);
+	b_len = string_length(b);
+	assert(b_len == c_len);
 
 	if (DEBUG) {
 		a_len = string_length(a);
@@ -87,40 +77,15 @@ bool string_swap(char a[], char b[])
 		b_len = string_length(b);
 		arc_print(b, b_len, "b");
 	}
-
-	return is_swaped;
 }
 
-void dic_print(struct dic note[], int len)
+void dic_swap(struct dic note[], int i, int j)
 {
-	int i = 0;
-
-	assert(len > 0);
-	printf("dictionary>\n word\t\tdefinition\n");
-
-	while (i <= len-1) {
-		printf("[ %s ]       [ %s ]\n", note[i].word, note[i].def);
-		++i;
-	}
-	assert(i == len);
-}
-
-bool dic_swap(struct dic note[], int i, int j)
-{
-	bool is_swaped = false, is_dic_swaped = false;
-
 	assert(i >= 0);
 	assert(j >= 0);
 
-	is_swaped = string_swap(note[i].word, note[j].word);
-	if (is_swaped) {
-		is_swaped = string_swap(note[i].def, note[j].def);
-		if (is_swaped) {
-			is_dic_swaped = true;
-		}
-	}
-
-	return is_dic_swaped;
+	string_swap(note[i].word, note[j].word);
+	string_swap(note[i].def, note[j].def);
 }
 
 bool string_compare(char a[], char b[])
@@ -141,7 +106,6 @@ bool string_compare(char a[], char b[])
 void dic_sort(struct dic note[], int len)
 {
 	int i;
-	bool is_compared = true, is_swaped = false;
 
 	assert(len > 0);
 
@@ -149,18 +113,28 @@ void dic_sort(struct dic note[], int len)
 		int j;
 
 		for (j = i+1; j <= len-1; ++j) {
+			bool is_compared = false;
 
 			is_compared = string_compare(note[i].word, note[j].word);
-
-			// true condition (a[i] > a[j])
-			if (!is_compared) {
-				is_swaped = dic_swap(note, i, j);
-				if (!is_swaped) {
-					printf("chk swap\n");
-				}
+			if (!is_compared) {						/* condition (note[i].word > note[j].word)*/
+				dic_swap(note, i, j);					/* swapped occurred */
 			}
 		}
 	}
+}
+
+void dic_print(struct dic note[], int len)
+{
+	int i = 0;
+
+	assert(len > 0);
+	printf("dictionary>\n word\t\tdefinition\n");
+
+	while (i <= len-1) {
+		printf("[ %s ]       [ %s ]\n", note[i].word, note[i].def);
+		++i;
+	}
+	assert(i == len);
 }
 
 void dic_read_line(struct dic note[], int pos)
