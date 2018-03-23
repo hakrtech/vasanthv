6c047f54b32e9fd82fd16108d2d3adab8101b7bf
Vasantharaja Valaiyapathi <vasanthcuddalore@gmail.com>
2018-03-23 18:11:14 +0530

    #7 c11 eg02 pointer char modified

--- kochan/c11/eg/eg02.c
+++ kochan/c11/eg/eg02.c
@@ -7,16 +7,17 @@
 int main(void)
 {
 	char c = 'Q';
-	char *char_val = &c;
+	char *char_ptr = &c;	/* char_ptr point to variable c,
+				   because variable c referenced by char_ptr*/
 
 	printf("basic character pointers>\n");
-	printf("character [ %c ] pointer value [ %c ]\n", c, *char_val);
+	printf("character [ %c ] pointer char [ %c ]\n", c, *char_ptr);
 
 	c = '/';
-	printf("character [ %c ] pointer value [ %c ]\n", c, *char_val);
+	printf("character [ %c ] pointer char [ %c ]\n", c, *char_ptr);
 
-	*char_val = '@';
-	printf("character [ %c ] pointer value [ %c ]\n", c, *char_val);
+	*char_ptr = '@';
+	printf("character [ %c ] pointer char [ %c ]\n", c, *char_ptr);
 
 	return 0;
 }
